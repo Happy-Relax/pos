ad57bcb98d25171ab76080431779f5758144934b
liujia9105 <305092548@qq.com>
2015-12-09 09:52:48 +0800

    完成测试程序

--- pos_v1/spec/main-spec.js
+++ pos_v1/spec/main-spec.js
@@ -112,10 +112,10 @@ describe('pos', function () {
 
 
 
-            var expectText =[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3}
+            var promotion_priceItems =[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3}
                               ,{count:2,name:'荔枝',price:15,unit:'斤',promotionprice:0}
                               ,{count:3,name:'方便面',price:4.5,unit:'袋',promotionprice:4.5}];
-            var CartItems=[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3,totalprice:12}
+            var expectText=[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3,totalprice:12}
                              ,{count:2,name:'荔枝',price:15,unit:'斤',promotionprice:0,totalprice:30}
                              ,{count:3,name:'方便面',price:4.5,unit:'袋',promotionprice:4.5,totalprice:9}];
             var actual=total(promotion_priceItems);
@@ -126,14 +126,119 @@ describe('pos', function () {
      it("Price(CartItems)", function () {
 
 
-
-            var expectText =
-            var CartItems=[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3,totalprice:12}
-                            ,{count:2,name:'荔枝',price:15,unit:'斤',promotionprice:0,totalprice:30}
+            var l_promotions=loadPromotions()[0].barcodes
+            var expectText =[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3,totalprice:12}
+                             ,{count:2,name:'荔枝',price:15,unit:'斤',promotionprice:0,totalprice:30}
                             ,{count:3,name:'方便面',price:4.5,unit:'袋',promotionprice:4.5,totalprice:9}];
+            var CartItems=[ {barcode:'ITEM000001',count:5,name:'雪碧',price:3,unit:'瓶'}
+                           ,{barcode:'ITEM000003',count:2,name:'荔枝',price:15,unit:'斤'}
+                           ,{barcode:'ITEM000005',count:3,name:'方便面',price:4.5,unit:'袋'}];
 
-            var actual=total(promotion_priceItems);
+            var actual=Price(CartItems);
 
             expect(actual).toEqual(expectText);
+     });
+
+     it("Gift(priceItems)", function () {
+
+
+            var l_promotions=loadPromotions()[0].barcodes
+            var expectText =[ {name:'雪碧',promotioncount:1,unit:'瓶'},{name:'方便面',promotioncount:1,unit:'袋'}];
+            var priceItems=[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3,totalprice:12}
+                            ,{count:2,name:'荔枝',price:15,unit:'斤',promotionprice:0,totalprice:30}
+                             ,{count:3,name:'方便面',price:4.5,unit:'袋',promotionprice:4.5,totalprice:9}];
+
+            var actual=Gift(priceItems);
+
+            expect(actual).toEqual(expectText);
+     });
+
+
+     it("Sum(priceItems)", function () {
+
+
+            var l_promotions=loadPromotions()[0].barcodes
+            var expectText ={total:51,save:7.5};
+            var priceItems=[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3,totalprice:12}
+                            ,{count:2,name:'荔枝',price:15,unit:'斤',promotionprice:0,totalprice:30}
+                             ,{count:3,name:'方便面',price:4.5,unit:'袋',promotionprice:4.5,totalprice:9}];
+
+            var actual=Sum(priceItems);
+
+            expect(actual).toEqual(expectText);
+     });
+
+     it("PrintAllItems(priceItems)", function () {
+
+            spyOn(console, 'log');
+
+            var expectText ='***<没钱赚商店>购物清单***\n' +
+                             '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+                             '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+                             '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n';
+            var priceItems=[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3,totalprice:12}
+                            ,{count:2,name:'荔枝',price:15,unit:'斤',promotionprice:0,totalprice:30}
+                             ,{count:3,name:'方便面',price:4.5,unit:'袋',promotionprice:4.5,totalprice:9}];
+
+           console.log(PrintAllItems(priceItems));
+
+            expect(console.log).toHaveBeenCalledWith(expectText);
+     });
+
+     it("PrintGiftItems(GiftItems)", function () {
+
+            spyOn(console, 'log');
+
+            var expectText ='----------------------\n' +
+                             '挥泪赠送商品：\n' +
+                             '名称：雪碧，数量：1瓶\n' +
+                             '名称：方便面，数量：1袋\n';
+            var GiftItems=[ {name:'雪碧',promotioncount:1,unit:'瓶'},{name:'方便面',promotioncount:1,unit:'袋'}]
+
+           console.log(PrintGiftItems(GiftItems));
+
+            expect(console.log).toHaveBeenCalledWith(expectText);
+     });
+
+     it("PrintSum(Receipt)", function () {
+
+            spyOn(console, 'log');
+
+            var expectText ='----------------------\n' +
+                        '总计：51.00(元)\n' +
+                         '节省：7.50(元)\n' +
+                          '**********************';
+            var Receipt={total:51,save:7.5};
+
+           console.log(PrintSum(Receipt));
+
+            expect(console.log).toHaveBeenCalledWith(expectText);
+     });
+
+
+     it("Print(priceItems,GiftItems,Receipt)", function () {
+
+            spyOn(console, 'log');
+
+            var expectText ='***<没钱赚商店>购物清单***\n' +
+                                        '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+                                        '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+                                        '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+                                        '----------------------\n' +
+                                        '挥泪赠送商品：\n' +
+                                        '名称：雪碧，数量：1瓶\n' +
+                                        '名称：方便面，数量：1袋\n' +
+                                        '----------------------\n' +
+                                        '总计：51.00(元)\n' +
+                                        '节省：7.50(元)\n' +
+                                        '**********************';
+            var Receipt={total:51,save:7.5};
+            var GiftItems=[ {name:'雪碧',promotioncount:1,unit:'瓶'},{name:'方便面',promotioncount:1,unit:'袋'}];
+            var priceItems=[ {count:5,name:'雪碧',price:3,unit:'瓶',promotionprice:3,totalprice:12}
+                                        ,{count:2,name:'荔枝',price:15,unit:'斤',promotionprice:0,totalprice:30}
+                                         ,{count:3,name:'方便面',price:4.5,unit:'袋',promotionprice:4.5,totalprice:9}];
+           console.log(Print(priceItems,GiftItems,Receipt));
+
+            expect(console.log).toHaveBeenCalledWith(expectText);
      });
 });
